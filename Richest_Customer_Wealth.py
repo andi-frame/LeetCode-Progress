ba1a4d44d7aeb4f63269a7a93613eead7a37d948
andi-frame <andifarhan1094@gmail.com>
2023-08-24 17:41:24 +0700

    1 line edition (more effecient)

--- Richest_Customer_Wealth.py
+++ Richest_Customer_Wealth.py
@@ -1,14 +1,6 @@
 class Solution(object):
     def maximumWealth(self, accounts):
-        max = 0
-        for m in accounts:
-            sum = 0
-            for n in m:
-                sum += n
-            m = sum
-            if m > max:
-                max = m
-        return max
+        return max(sum(m) for m in accounts)
                 
         """
         :type accounts: List[List[int]]
