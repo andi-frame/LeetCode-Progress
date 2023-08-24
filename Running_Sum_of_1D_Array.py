8b6bf3a88282d15b5f21569a6cf38e58c7ecd6e2
andi-frame <132210040+andi-frame@users.noreply.github.com>
2023-08-24 15:48:43 +0700

    Update One pass, in place solution

--- Running_Sum_of_1D_Array.py
+++ Running_Sum_of_1D_Array.py
@@ -1,13 +1,8 @@
 class Solution(object):
     def runningSum(self, nums):
-        result = []
-        num = int()
-        for i in range(len(nums)):
-            for j in range(0, i+1):
-                num += nums[j]
-            result.append(num)
-            num = 0
-        return result
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+        return nums
             
         """
         :type nums: List[int]
