da96d2c62c8ee6d5d7c86cd6faba3cf37d39de80
andi-frame <132210040+andi-frame@users.noreply.github.com>
2023-08-30 23:41:19 +0700

    Update Longest_Common_Prefix.py || More simple code

--- Longest_Common_Prefix.py
+++ Longest_Common_Prefix.py
@@ -1,28 +1,11 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        if len(strs) == 1:
-            return strs[0]
-        longest_common_prefix = ""
-        long_prefix = True
-        for i in range(len(strs[0])):
-            for str in strs[1:len(strs)]:
-                try:
-                    str[i]
-                except:
-                    return longest_common_prefix
-                if strs[0][i] == str[i]:
-                    long_prefix *= True
-                else:
-                    long_prefix *= False
-            
-            if long_prefix:
-                longest_common_prefix += str[i]
-        
-        return longest_common_prefix
-                
-
-
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
+class Solution:
+    def longestCommonPrefix(self, v: List[str]) -> str:
+        ans=""
+        v=sorted(v)
+        first=v[0]
+        last=v[-1]
+        for i in range(min(len(first),len(last))):
+            if(first[i]!=last[i]):
+                return ans
+            ans+=first[i]
+        return ans
