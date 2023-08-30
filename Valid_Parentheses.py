959145af8d44fc05f81e2d8239c867ca92f2e383
andi-frame <132210040+andi-frame@users.noreply.github.com>
2023-08-30 18:50:04 +0700

    Update Valid_Parentheses.py || Now the algorithm is correct

--- Valid_Parentheses.py
+++ Valid_Parentheses.py
@@ -1,15 +1,18 @@
 class Solution(object):
     def isValid(self, s):
         memory = []
+        num = len(s)
         for c in s:
             if c in "([{":
                 memory.append(c)
+                num -= 1
             if not memory or len(s)%2 != 0:
                 return False
             if (c == ')' and memory[-1] == '(') or (c == ']' and memory[-1] == '[') or (c == '}' and memory[-1] == '{'):
                 memory.pop()
+                num -= 1
       
-        if len(memory) == 0:
+        if num == 0 and len(memory) == 0:
             return True
         else:
             return False
