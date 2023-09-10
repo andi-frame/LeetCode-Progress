b7f3c0ed1fb0c3abbd7817fb8d4b5f35c1a240ab
andi-frame <132210040+andi-frame@users.noreply.github.com>
2023-09-11 00:11:19 +0700

    Update Plus_One.py || Not mine, surrender.

--- Plus_One.py
+++ Plus_One.py
@@ -1,14 +1,11 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        if digits[-1] == 9:
-            before = digits.index(digits[-1]-1, 0, len(digits))
-            n = before[-1]
-            
-            for i in range(len(digits)-1,-1,-1):
-                digits[i] = 0
-            digits.insert(0, 1)
+        if digits[-1] < 9:
+            digits[-1] += 1
             return digits
-
-        digits[-1] += 1
-
-        return digits
+        elif len(digits) == 1 and digits[0] == 9:
+            return [1, 0]
+        else:
+            digits[-1] = 0
+            digits[0:-1] = self.plusOne(digits[0:-1])
+            return digits
